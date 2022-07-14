8ca03a76561a06ee72b42e1725d6ff9e7f16d882
tatienloc <107953189+tatienloc@users.noreply.github.com>
2022-07-14 12:08:06 +0700

    add statusbar

--- models/purchase_request.py
+++ models/purchase_request.py
@@ -5,14 +5,19 @@ class Purchaserequest(models.Model):
     _description = "Purchase request"
 
     department_id = fields.Many2one("hr.department", string="Department" , required=True)
-    request_id = fields.Many2one("res.users")
-    approver_id = fields.Many2one("res.users", string="approver")
-    date = fields.Date(string="Date", default=fields.Date.today() , readonly=True)
-    date_approve = fields.Date(string="Date approve")
-    request_line_ids =fields.One2many("purchase.request.line" , "request_id")
-    description = fields.Text(string="Description")
-    state = fields.Selection([("Draft", "draft"), ("Wait", "wait"), ("Approved", "approved"), ("Cancel", "cancel")],
-                             default="Draft")
+    request_id = fields.Many2one("res.users" , required=True)
+    approver_id = fields.Many2one("res.users", string="approver" , required=True)
+    date = fields.Date(string="Date", default=fields.Date.today() , readonly=True )
+    date_approve = fields.Date(string="Date approve" , required=True)
+    request_line_ids =fields.One2many("purchase.request.line" , "request_id" , required=True)
+    description = fields.Text(string="Description" )
+    state = fields.Selection([("draft", "Draft"), ("wait", "Wait"), ("approved", "Approved"), ("cancel", "Cancel")],
+                             default="draft" , required=True)
     total_qty = fields.Integer(string="Total qty")
     total_amount = fields.Float(string="Total amount")
+
+    @api.returns('mail.message', lambda value: value)
+    def request_user(self):
+        self.write({'state': 'wait'})
+        return {}
 
